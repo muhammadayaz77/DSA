0703069e79ef03ce1bc2421f3f57c3f8074a3936
Muhammad Ayaz <muhammadayaz22757@gmail.com>
2025-07-19 20:54:59 +0500

    add stack implementation in JavaScript and TypeScript

---
+++ Beginner Level/Stack.ts
@@ -0,0 +1,53 @@
+
+
+let arr : number[] = [1,2,3,4,5,6,7,8,9];
+let len = arr.length;
+
+console.log(arr.length)
+
+let stack = (num:number) => {
+  if(arr.length == 0){
+    arr.length = 0
+  }
+  arr[len] = num;
+  len++;
+  console.log(arr)
+}
+
+let pop = () => {
+  if(len <= 0){
+    console.log("Stack is already empty")
+  }
+  else{
+    len--;
+    arr.length = len;
+    console.log('Pop : ',arr);
+  }
+}
+
+stack(10)
+stack(11)
+stack(12)
+
+pop()
+pop()
+pop()
+pop()
+pop()
+pop()
+pop()
+pop()
+pop()
+pop()
+pop()
+pop()
+stack(2)
+stack(2)
+stack(2)
+pop()
+pop()
+pop()
+pop()
+pop()
+stack(1)
+console.log(arr.length)

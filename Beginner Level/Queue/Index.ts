1a7a12f47ca95013642d736a1e7605b4c0159d91
Muhammad Ayaz <muhammadayaz22757@gmail.com>
2025-07-20 10:59:05 +0500

    add queue implementation with enqueue and dequeue methods

--- Beginner Level/Queue/Index.ts
+++ Beginner Level/Queue/Index.ts
@@ -3,4 +3,28 @@
 
 let arr:number[] = [1,2,3,4,5];
 
-
+let firstLoc = 0;
+let lastLoc = arr.length;
+// console.log(lastLoc)
+let enqueue = (num : number) => {
+  arr[lastLoc] = num
+  lastLoc++;
+}
+let dequeue = () => {
+  if(arr.length <= 0){
+    console.log('already empty');
+  }
+  else{
+    for(let i = 0;i<arr.length;i++){
+      arr[i] = arr[i+1]
+    }
+    lastLoc--;
+    arr.length = lastLoc;
+  }
+}
+enqueue(10)
+enqueue(10)
+enqueue(10)
+dequeue()
+dequeue()
+console.log(arr)
